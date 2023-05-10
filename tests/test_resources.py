a41688a6ad528955a9ee190a3e1e23a23b54f4ed
TheByronHimes <TheByronHimes@gmail.com>
2023-05-10 13:25:14 +0000

    Extend test_basic_query_and_load

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -50,12 +50,54 @@ async def test_basic_query_and_load(joint_fixture: JointFixture):  # noqa: F811
         class_name="DatasetEmbedded",
         query="stoncher",
         filters=[models.Filter(key="field1", value="Beta")],
-        # filters=[],
         skip=0,
         limit=10,
     )
 
     assert len(results_filtered) == 1
+
+    # get all the documents in the collection
+    results_all = await query_handler.handle_query(
+        class_name="DatasetEmbedded",
+        query="",
+        filters=[],
+        skip=0,
+        limit=0,
+    )
+
+    # define and load a new resource
+    resource = models.Resource(
+        id_="jf2jl-dlasd82",
+        content={
+            "has_feature": {"feature_name": "added_resource", "id": "98u44-f4jo4"}
+        },
+    )
+
+    await query_handler.load_resource(resource=resource, class_name="DatasetEmbedded")
+
+    # make sure the new resource is added to the collection
+    results_after_load = await query_handler.handle_query(
+        class_name="DatasetEmbedded", query="", filters=[], skip=0, limit=0
+    )
+    assert len(results_after_load) - len(results_all) == 1
+
+    target_search = await query_handler.handle_query(
+        class_name="DatasetEmbedded",
+        query="added_resource",
+        filters=[],
+        skip=0,
+        limit=0,
+    )
+    assert len(target_search) == 1
+    validated_resource = models.Resource(**target_search[0])
+    assert validated_resource.id_ == resource.id_
+    assert validated_resource.content == resource.content
+
+    # make sure limit works
+    results_limited = await query_handler.handle_query(
+        class_name="DatasetEmbedded", query="", filters=[], skip=0, limit=2
+    )
+    assert len(results_limited) == 2
 
 
 @pytest.mark.skip
@@ -63,4 +105,4 @@ async def test_absent_resource(joint_fixture: JointFixture):  # noqa: F811
     """Make sure we get an error when looking for a resource type that doesn't exist"""
     query_handler = await joint_fixture.container.query_handler()
     with pytest.raises(DaoNotFoundError):
-        await query_handler.handle_query(class_name="raccoon")
+        await query_handler.handle_query(class_name="does_not_exist")
