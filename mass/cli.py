3c30691356bbab91514f5e9a682f1255dd5737f8
TheByronHimes <TheByronHimes@gmail.com>
2023-07-27 10:58:42 +0000

    Fork entrypoint via CLI commands

--- mass/cli.py
+++ mass/cli.py
@@ -15,7 +15,24 @@
 
 """Entrypoint of the package"""
 
-from mass.cli import cli
+import asyncio
 
-if __name__ == "__main__":
-    cli()
+import typer
+
+from mass.main import consume_events, run_rest
+
+cli = typer.Typer()
+
+
+@cli.command(name="run-rest")
+def sync_run_api():
+    """Run the HTTP REST API."""
+
+    asyncio.run(run_rest())
+
+
+@cli.command(name="consume-events")
+def sync_consume_events(run_forever: bool = True):
+    """Run an event consumer listening to the specified topic."""
+
+    asyncio.run(consume_events(run_forever=run_forever))
