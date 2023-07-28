d9eb2b9878ac1d1e2341012f2f3f2ca0b0269bd5
TheByronHimes <TheByronHimes@gmail.com>
2023-07-28 14:47:08 +0000

    Log errors in consumer

--- mass/adapters/inbound/event_sub.py
+++ mass/adapters/inbound/event_sub.py
@@ -14,14 +14,35 @@
 # limitations under the License.
 
 """Event subscriber details for searchable resource events"""
+import logging
+
 import ghga_event_schemas.pydantic_ as event_schemas
-from ghga_event_schemas.validation import get_validated_payload
+from ghga_event_schemas.validation import (
+    EventSchemaValidationError,
+    get_validated_payload,
+)
 from hexkit.custom_types import Ascii, JsonObject
 from hexkit.protocols.eventsub import EventSubscriberProtocol
 from pydantic import BaseSettings, Field
 
 from mass.core.models import Resource
-from mass.ports.inbound.query_handler import QueryHandlerPort
+from mass.ports.inbound.query_handler import (
+    ClassNotConfiguredError,
+    DeletionFailedError,
+    QueryHandlerPort,
+)
+
+CLASS_NOT_CONFIGURED_LOG_MSG = "Class with name %s not configured."
+
+DELETION_FAILED_LOG_MSG = (
+    "Failed to delete resource with ID '%s' because "
+    + "no match was found in the database."
+)
+SCHEMA_VALIDATION_ERROR_LOG_MSG = "Failed to validate event schema for '%s'"
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 
 class EventSubTranslatorConfig(BaseSettings):
@@ -65,40 +86,66 @@ class EventSubTranslator(EventSubscriberProtocol):
         """Delete the specified resource.
 
         Validates the schema, then makes a call to the query handler with the payload.
+        If there's an error during schema validation, we're done (log and exit func).
         """
-        validated_payload = get_validated_payload(
-            payload=payload, schema=event_schemas.SearchableResourceInfo
-        )
-        await self._query_handler.delete_resource(
-            resource_id=validated_payload.accession,
-            class_name=validated_payload.class_name,
-        )
+        try:
+            validated_payload = get_validated_payload(
+                payload=payload, schema=event_schemas.SearchableResourceInfo
+            )
+
+            try:
+                await self._query_handler.delete_resource(
+                    resource_id=validated_payload.accession,
+                    class_name=validated_payload.class_name,
+                )
+            except DeletionFailedError:
+                logger.warning(DELETION_FAILED_LOG_MSG, validated_payload.accession)
+            except ClassNotConfiguredError:
+                logger.warning(
+                    CLASS_NOT_CONFIGURED_LOG_MSG, validated_payload.class_name
+                )
+        except EventSchemaValidationError:
+            logger.error(
+                SCHEMA_VALIDATION_ERROR_LOG_MSG,
+                event_schemas.SearchableResourceInfo.__name__,
+            )
 
     async def _handle_upsertion(self, *, payload: JsonObject):
         """Load the specified resource.
 
         Validates the schema, then makes a call to the query handler with the payload.
+        If there's an error during schema validation, we're done (log and exit func).
         """
-        validated_payload = get_validated_payload(
-            payload=payload, schema=event_schemas.SearchableResource
-        )
+        try:
+            validated_payload = get_validated_payload(
+                payload=payload, schema=event_schemas.SearchableResource
+            )
+            resource = Resource(
+                id_=validated_payload.accession,
+                content=validated_payload.content,
+            )
 
-        resource = Resource(
-            id_=validated_payload.accession,
-            content=validated_payload.content,
-        )
-
-        await self._query_handler.load_resource(
-            resource=resource,
-            class_name=validated_payload.class_name,
-        )
+            try:
+                await self._query_handler.load_resource(
+                    resource=resource,
+                    class_name=validated_payload.class_name,
+                )
+            except ClassNotConfiguredError:
+                logger.warning(
+                    CLASS_NOT_CONFIGURED_LOG_MSG, validated_payload.class_name
+                )
+        except EventSchemaValidationError:
+            logger.error(
+                SCHEMA_VALIDATION_ERROR_LOG_MSG,
+                event_schemas.SearchableResource.__name__,
+            )
 
     async def _consume_validated(
         self,
         *,
         payload: JsonObject,
         type_: Ascii,
-        topic: Ascii  # pylint: disable=unused-argument
+        topic: Ascii,  # pylint: disable=unused-argument
     ) -> None:
         """Consumes an event"""
         if type_ == self._config.resource_deletion_event_type:
