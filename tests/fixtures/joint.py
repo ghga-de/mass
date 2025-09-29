f55800ad70c654a956bacec5864b07e64d63eb74
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2025-09-29 15:03:47 +0200

    Update Python and dependencies (#45)

--- tests/fixtures/joint.py
+++ tests/fixtures/joint.py
@@ -178,7 +178,7 @@ class JointFixture:
 @pytest_asyncio.fixture()
 async def joint_fixture(
     mongodb: MongoDbFixture, kafka: KafkaFixture
-) -> AsyncGenerator[JointFixture, None]:
+) -> AsyncGenerator[JointFixture]:
     """Function scoped joint fixture for API-level integration testing."""
     # merge configs from different sources with the default one:
     config = get_config(sources=[mongodb.config, kafka.config], kafka_enable_dlq=True)
